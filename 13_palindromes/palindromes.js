6f928e5a6b77b2649eaf6c3d4ff69f21e58eedaf
jaredmasters <mastersjared3@gmail.com>
2025-11-01 16:49:08 -0700

    complete: 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+function isPunctuation(char) {
+    return char == "!" || char == "," || char == "." || char == "?";
+}
 
+const palindromes = function (line) {
+    line = line.trim().toLowerCase();
+    let regular = "";
+    let reversed = "";
+    for (let i = 0; i < line.length; ++i) {
+        const cRegular = line.at(i);
+        const cReversed = line.at(line.length - 1 - i);
+
+        if (!isPunctuation(cRegular) && cRegular != " ") {
+            regular += cRegular;
+        }
+        
+        if (!isPunctuation(cReversed) && cReversed != " ") {
+            reversed += cReversed;
+        }
+    }
+    return regular == reversed;
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
