8692e14d80cf4da73b0b0b38e1c86c50d0549e2c
jaredmasters <mastersjared3@gmail.com>
2025-11-01 17:13:12 -0700

    complete: 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((maxAgePerson, person) => {
+        const currentYear = (new Date()).getFullYear();
+        const personYearOfDeath = person.yearOfDeath != undefined ? person.yearOfDeath : currentYear;
+        const maxYearOfDeath = maxAgePerson.yearOfDeath != undefined ? maxAgePerson.yearOfDeath : currentYear;
 
+        return  personYearOfDeath - person.yearOfBirth >
+                maxYearOfDeath - maxAgePerson.yearOfBirth ?
+                person : maxAgePerson;
+    }, arr[0])
 };
 
 // Do not edit below this line
