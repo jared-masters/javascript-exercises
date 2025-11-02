905977aa6665e239e462ca43db35972fc3735652
jaredmasters <mastersjared3@gmail.com>
2025-11-01 17:01:40 -0700

    complete: 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    let first = 1;
+    let second = 1;
+    for (let i = 2; i < n; ++i) {
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+    return second;
 };
 
 // Do not edit below this line
